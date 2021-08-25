738ef10aab2aa159a64f0d52b00a94ed1cc0fb22
stefanatanasov1 <stefanatanasov1@users.github.com>
2021-08-25 21:36:28 +0300

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,8 +4,8 @@ import { Http,Response} from '@angular/http';
 @Injectable()
 export class ProductService {
 
-  private _AlbumUrl : string = '../assets/album.json';
-  
+  private _albumUrl = '../assets/album.json';
+
   constructor(private _http: Http) {
 
    }
