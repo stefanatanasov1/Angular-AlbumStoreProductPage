2c29af40e708db530107857cf588cea18aee7e94
stefanatanasov1 <stefanatanasov1@users.github.com>
2021-08-26 11:40:42 +0300

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,14 +5,15 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-  private _albumUrl = '../assets/album/json';
+  private  _albumUrl = '../assets/album/json';
 
   constructor (private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl)
+    return this._http.get(this._albumUrl).map((response)=>response.json());
   }
 
 }
 
 
+  
